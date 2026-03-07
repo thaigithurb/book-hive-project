1a39009eceec9823658af7b25ca802297fd34b9f
thaigithurb <thaivietluong2005@gmail.com>
2026-03-07 19:11:00 +0700

    Update page.tsx

--- frontend/src/app/(client)/books/page.tsx
+++ frontend/src/app/(client)/books/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import { Book } from "@/app/interfaces/book.interface";
